3f76fe7576ee94396eb0a7fb192c6c93db3c9207
Jovan <j.miljkovic95@gmail.com>
2020-12-23 18:54:32 +0100

    functionalities with contact numbers

--- app/Services/ContactService.php
+++ app/Services/ContactService.php
@@ -2,7 +2,11 @@
 
 namespace App\Services;
 
+use App\Repository\ContactNumberRepository;
 use App\Repository\ContactRepository;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
 
 class ContactService{
 
@@ -10,10 +14,15 @@ class ContactService{
      * @var ContactRepository
      */
     private $contactRepository;
+    /**
+     * @var ContactNumberRepository
+     */
+    private $contactNumberRepository;
 
-    public function __construct(ContactRepository $contactRepository)
+    public function __construct(ContactRepository $contactRepository, ContactNumberRepository $contactNumberRepository)
     {
         $this->contactRepository = $contactRepository;
+        $this->contactNumberRepository = $contactNumberRepository;
     }
 
     public function getAllContacts()
@@ -23,13 +32,33 @@ class ContactService{
 
     public function createContacts($attributes)
     {
-        foreach ($attributes['contacts'] as $attribute)
-        {
-           $contact = $this->contactRepository->create($attribute);
+        DB::beginTransaction();
+
+        try {
+
+            foreach ($attributes['contacts'] as $attribute) {
+
+               $contact = $this->contactRepository->create($attribute);
+
+               foreach ($attribute['phones'] as $phone){
+
+                   $this->contactNumberRepository->create($contact->id, $phone['type'] , $phone['number']);
+
+               }
+
+            }
+
+            DB::commit();
+
+            return true;
+
+        } catch (Exception $e) {
+
+            DB::rollback();
+
+            return response(["error" => $e->getMessage()]);
 
         }
-
-        return true;
 
     }
 
